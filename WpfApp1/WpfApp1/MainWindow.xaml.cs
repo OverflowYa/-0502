91c59ed658b54a2f4969a1e4c8335044fcd6ee53
OverflowYa <120401794+OverflowYa@users.noreply.github.com>
2023-06-06 18:02:32 +0300

    Add files via upload

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MenuItem menuItem = sender as MenuItem;
+            Button button = sender as Button;
+            if (menuItem != null)
+            {
+                if (menuItem.Header.ToString() == "_Close")
+                {
+                    this.Close();
+                }
+                else if (menuItem.Header.ToString() == "_Change Background Color")
+                {
+                    this.Background = System.Windows.Media.Brushes.Black; // изменение фона на черный
+                }
+                else if (menuItem.Header.ToString() == "_About")
+                {
+                    MessageBox.Show("Developed by OverflowYa(Есипов Влад)");
+                }
+                statusText.Text = "Clicked " + menuItem.Header.ToString();
+            }
+            else if (button != null)
+            {
+                if (button.Content.ToString() == "_Close")
+                {
+                    this.Close();
+                }
+                else if (button.Content.ToString() == "_Change Background Color")
+                {
+                    this.Background = System.Windows.Media.Brushes.Black; // изменение фона на черный
+                }
+                else if (button.Content.ToString() == "_About")
+                {
+                    MessageBox.Show("Developed by OverflowYa(Есипов Влад)");
+                }
+                statusText.Text = "Clicked " + button.Content.ToString();
+            }
+        }
+    }
+}
